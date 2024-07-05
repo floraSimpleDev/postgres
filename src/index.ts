8b1a98b4a89b20fddb6014e2e53f6d05740b018a
Flora Li <samantha20200604@gmail.com>
2024-07-05 22:49:39 +1000

    different context syntax in ApolloServer 4

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,22 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { schema } from "./schema";
 import typeormConfig from "./typeorm.config";
+import { Context } from "./types/Context";
 
 // set up ApolloServer
 const boot = async () => {
-  // connect psql database
-  await typeormConfig.initialize();
+  // connect psql database, pass connect into _context
+  const connect = await typeormConfig.initialize();
 
-  const server = new ApolloServer({
+  // different syntax in ApolloServer 4
+  const server = new ApolloServer<Context>({
     schema,
   });
 
   // listen server at port 5000
   const { url } = await startStandaloneServer(server, {
     listen: { port: 5000 },
+    context: async () => ({ connect }), // different syntax in ApolloServer 4
   });
   console.log(`I'm listening at ${url}`);
 };
