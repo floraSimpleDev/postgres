b4e4ccdcb5b4d0a8b4ef0637fa91257c0a79f5bd
Flora Li <samantha20200604@gmail.com>
2024-07-06 00:06:35 +1000

    set up register auth

---
+++ src/graphql/Auth.ts
@@ -0,0 +1,51 @@
+import { extendType, objectType, stringArg, nonNull } from "nexus";
+import { Context } from "../types/Context";
+import { User } from "../entities/User";
+import argon2 from "argon2";
+
+// init Auth type
+export const AuthType = objectType({
+  name: "AuthType",
+  definition(type) {
+    type.nonNull.string("token");
+    type.nonNull.field("user", {
+      type: "User",
+    });
+  },
+});
+
+export const AuthMutation = extendType({
+  type: "Mutation",
+  definition(type) {
+    type.nonNull.field("register", {
+      type: "AuthType",
+      args: {
+        username: nonNull(stringArg()),
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      async resolve(_parent, args, context: Context, _info) {
+        const { username, email, password } = args;
+        const hashedPassword = await argon2.hash(password);
+        let user;
+        let token;
+
+        try {
+          const result = await context.connect
+            .createQueryBuilder()
+            .insert()
+            .into(User)
+            .values({
+              username,
+              password: hashedPassword,
+              email,
+            })
+            .returning("*")
+            .execute();
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    });
+  },
+});
