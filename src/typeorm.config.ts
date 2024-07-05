6d5b06fc984d59346d1e90defa517461ee50728a
Flora Li <samantha20200604@gmail.com>
2024-07-05 19:30:47 +1000

    connect psql database

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import { DataSource } from "typeorm";
+import { Product } from "./entities/Product";
 
 dotenv.config();
 
+// entities added to DataSource, and synchronize every change of entities
 export default new DataSource({
   type: "postgres",
   url: process.env.CONNECTION_STRING,
+  entities: [Product],
+  synchronize: true, // only use it in development stage
 });
+/* query at psql -> Schemas -> Tables -> product
+select * from product 
+*/
